78b36919d28b1e930a8b004f957cd573c8f7fbc6
Scott Herrmann - GitHub Hiddent Email <1109038+daborg69@users.noreply.github.com>
2021-01-15 15:55:58 -0500

    Emailing

---
+++ src/PrinterMgr/Emailing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrinterMgr
+{
+	class Emailing
+	{
+		public Emailing () {}
+
+
+		public bool Status () {
+			return true;
+		}
+	}
+}
